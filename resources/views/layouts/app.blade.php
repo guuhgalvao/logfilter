0bfd5f3a63b9b9fc9cfb9f69fc31661f3ec72776
Gustavo Galvão <gu.afgalvao@gmail.com>
2020-05-17 14:04:25 -0300

    last update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,9 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{{ asset('img/favicon.png') }}}">
+
     <!-- Scripts -->
     <script src="{{ asset('js/jquery.min.js') }}"></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
